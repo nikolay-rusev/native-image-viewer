2419cd387462384865ac4ad5d8260e641eb3cb7c
n.rusev <n.rusev@tinqin.com>
2025-07-17 11:31:04 +0300

    remove commented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-
 import './App.css';
-// import ImagePDFViewer from "./components/ImagePDFViewer";
-// import ImageViewerLikePDF from "./components/ImageViewerLikePDF";
-import MyFlipBook from "./components/MyFlipBook";
 import SwiperViewer from "./components/SwiperViewer";
 
 function App() {
