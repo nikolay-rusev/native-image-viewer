8a2dd4c5277d49e10de8c1a6246a35e7d2b2828d
n.rusev <n.rusev@tinqin.com>
2025-07-17 17:03:41 +0300

    update images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import ImagePDFViewer from "./components/ImagePDFViewer";
+import ImageViewerLikePDF from "./components/ImageViewerLikePDF";
+import MyFlipBook from "./components/MyFlipBook";
 import SwiperViewer from "./components/SwiperViewer";
 
 function App() {
+  // return <ImagePDFViewer />;
+  // return <ImageViewerLikePDF />;
+  // return <MyFlipBook />;
   return <SwiperViewer />;
 }
 
