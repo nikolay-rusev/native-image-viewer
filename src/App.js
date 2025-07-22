802cc46642174e67a2b4b5941a292412d6d543d4
n.rusev <n.rusev@tinqin.com>
2025-07-22 14:52:36 +0300

    show only native viewer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import "./App.css";
-import SwiperViewer from "./components/SwiperViewer";
-import ZoomPanImage from "./components/ZoomPanImage";
 import NativeImageViewer from "./components/NativeImageViewer";
 
 function App() {
-  // return (
-  //   <ZoomPanImage
-  //     images={["images/img1.png", "images/img2.png", "images/img3.png"]}
-  //   />
-  // );
-  // return <SwiperViewer />;
-  return <NativeImageViewer />
+  return <NativeImageViewer />;
 }
 
 export default App;
